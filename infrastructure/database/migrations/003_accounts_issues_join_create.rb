1bfbe60232e53da36c119a715e620d5af8eb1c00
Vic Xu <xumingyo@gmail.com>
2018-05-14 00:33:08 +0800

    fix schema

--- infrastructure/database/migrations/003_accounts_issues_join_create.rb
+++ infrastructure/database/migrations/003_accounts_issues_join_create.rb
@@ -2,6 +2,11 @@ require 'sequel'
 
 Sequel.migration do 
     change do
-        create_join_table(collaborator_id: :accounts, issue_id: :issues)
+        create_table(:accounts_issues) do
+            foreign_key :collaborator_id, table: :accounts
+            foreign_key :issue_id, table: :issues, type: :uuid
+            primary_key [:collaborator_id, :issue_id]
+            index [:collaborator_id, :issue_id]
+        end
     end
 end
--- infrastructure/database/migrations/004_create_comment_table.rb
+++ infrastructure/database/migrations/004_create_comment_table.rb
@@ -4,7 +4,7 @@ Sequel.migration do
     change do 
         create_table(:comments) do
             uuid :id, primary_key: true
-            foreign_key :issue_id, table: :issues
+            foreign_key :issue_id, table: :issues, tpye: :uuid
             foreign_key :commenter_id, table: :accounts
 
             String :content_secure, null: false
