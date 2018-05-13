939d9822d15eb9efa058a5afd6ed8b0292bbe94f
Vic Xu <xumingyo@gmail.com>
2018-05-14 00:41:50 +0800

    fix db schema

--- infrastructure/database/migrations/006_create_feedback_table.rb
+++ infrastructure/database/migrations/006_create_feedback_table.rb
@@ -3,8 +3,8 @@ require 'sequel'
 Sequel.migration do
     change do
         create_table(:feedbacks) do
-            foreign_key :commenter_id, table: :accounts, type: :uuid
-            foreign_key :comment_id, table: :comments, tpye: :uuid
+            foreign_key :commenter_id, table: :accounts
+            foreign_key :comment_id, table: :comments, type: :uuid
             
             foreign_key :description_id, table: :feedback_descriptions
             primary_key :id
