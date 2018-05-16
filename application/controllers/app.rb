acc9bf2fc9616334108e7255dc9909cd9eb93032
Vic Xu <xumingyo@gmail.com>
2018-05-16 10:06:51 +0800

    add license

--- application/controllers/app.rb
+++ application/controllers/app.rb
@@ -7,8 +7,14 @@ module TalkUp
         plugin :multi_route
         plugin :halt
         plugin :all_verbs
+        
+        def secure_request?(routing)
+            routing.scheme.casecmp(Api.config.SECURE_SCHEME).zero?
+        end
 
         route do |routing|
+            response    ['Content_Type'] = 'application/json'
+            secure_request?(routing) || routing.halt(403, {message: 'TLS/SSL Requested'}.to_json)
             app = Api
 
             routing.root do 
